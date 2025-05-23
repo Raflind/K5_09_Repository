b8ee6c300713d9395a9b56ea3a7e5d5aa0923008
Raflind <18223038@std.stei.itb.ac.id>
2025-05-24 00:34:28 +0700

    feat Foods.java

---
+++ src/Items/Crops.java
@@ -0,0 +1,43 @@
+package Items;
+
+public class Crops extends Items {
+    private int daystoHarvest;
+    private boolean isWatered;
+    private int dayelapsed;
+
+    public Crops(String name, int sellPrice, int buyPrice, boolean isEdible, int itemID, int daystoHarvest) {
+        super(name, sellPrice, buyPrice, isEdible, itemID);
+        this.daystoHarvest = daystoHarvest;
+        this.isWatered = false;
+        this.dayelapsed = 0;
+    }
+
+    public void water() {
+        this.isWatered = true;
+    }
+
+    public void unwater() {
+        this.isWatered = false;
+    }
+    
+    public boolean isWatered() {
+        return isWatered;
+    }
+    
+    public int getDaystoHarvest() {
+        return daystoHarvest;
+    }
+    
+    public int getDayelapsed() {
+        return dayelapsed;
+    }
+    
+    public void incrementDayElapsed() {
+        this.dayelapsed++;
+    }
+    
+    public boolean isHarvestable() {
+        return dayelapsed >= daystoHarvest && isWatered;
+    }
+    
+}
---
+++ src/Items/Foods.java
@@ -0,0 +1,14 @@
+package Items;
+
+public class Foods extends Items {
+    private int energyPoints;
+
+    public Foods(String name, int sellPrice, int buyPrice, boolean isEdible, int itemID, int healthPoints, int energyPoints) {
+        super(name, sellPrice, buyPrice, isEdible, itemID);
+        this.energyPoints = energyPoints;
+    }
+
+    public int getEnergyPoints() {
+        return energyPoints;
+    }
+}
